355dd27021c390ade0f97b2fef8b990b5efb8fa5
Cogno <cognokill@gmail.com>
2018-04-04 23:19:17 +0200

    Unity Commit

---
+++ Assets/ShaderTests/FireIGuess.shader
@@ -0,0 +1,74 @@
+﻿Shader "Custom/FireIGuess"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_NoiseSpeed ("Noise Vertical Speed", float) = 1
+		_Bottom ("Bottom Color", Color) = (1,1,1,1)
+		_Top ("Top Color", Color) = (0,0,0,1)
+		_Saturate("Saturate Noise", Range(0,1)) = 30
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Transparent" "Queue" = "Transparent" }
+		LOD 100
+		ZWrite Off
+		Blend One One
+
+		Pass
+		{
+			
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _NoiseSpeed;
+			fixed4 _Bottom;
+			fixed4 _Top;
+			float _Saturate;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				
+				fixed4 noise = tex2D(_MainTex, float2(i.uv.x, i.uv.y + _Time[0] * _NoiseSpeed));
+				fixed gradient = lerp(_Top, _Bottom, i.uv.y);
+				float4 flame = saturate(noise.a * _Saturate);
+				// apply fog
+				//UNITY_APPLY_FOG(i.fogCoord, col);
+				float4 result = (flame + noise) * gradient;
+				return result;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Terrain/Grid.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Grid : MonoBehaviour {
+	
+	public int size;
+	public int height;
+	
+	public float squaresSide;
+	
+	public bool[,,] grid;
+	
+	private Mesh mesh;
+	private List<Vector3> verts;
+	private List<int> tri;
+	
+	//int mapWidth, int mapHeight, float scale, int seed, int octaves,float persistance, float lacunarity, Vector2 offset, NormalizeMode normalizeMode
+	[Range(0.1f,100f)]
+	public float scale;
+	public int seed;
+	[Range(1,5)]
+	public int octaves;
+	[Range(0.1f,10f)]
+	public float persistance;
+	[Range(0.001f,1f)]
+	public float lacunarity;
+	public Vector2 offset;
+	public Noise.NormalizeMode normalize;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnDrawGizmos(){
+		Setup();
+		float[,] noise = Noise.NoiseMapGenerator(size, size, scale, seed, octaves, persistance, lacunarity, offset, normalize);
+		for(int y = 0; y < size+1; y++){
+			for (int x = 0; x < size+1; x++)
+			{
+				float currentHeight = noise[x,y];
+				Vector3 pos = new Vector3(x,currentHeight,y);
+				verts.Add(pos);
+				if(x < size && y < size){
+					int t1 = x + y * size + y;
+					int t2 = t1 + 1;
+					int t3 = t1 + size + 1;
+					int t4 = t3 + 1;
+					tri.Add(t1);
+					tri.Add(t3);
+					tri.Add(t2);
+					tri.Add(t3);
+					tri.Add(t4);
+					tri.Add(t2);
+				}
+			}
+		}
+		Create();
+	}
+	
+	void Create(){
+		mesh.vertices = verts.ToArray();
+		mesh.triangles = tri.ToArray();
+		mesh.RecalculateNormals();
+		GetComponent<MeshFilter>().mesh = mesh;
+	}
+	
+	void Setup(){
+		grid = new bool[size+1,size+1,height+1];
+		
+		verts = new List<Vector3>();
+		tri = new List<int>();
+		
+		mesh = new Mesh();
+	}
+}
