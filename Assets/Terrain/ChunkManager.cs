666bfe93c27cc6018cbccd4597e4c2095bbbf26d
Cogno <cognokill@gmail.com>
2018-04-07 15:03:25 +0200

    Infinite Terrain + Normals Fix
    Fixato errore seam delle normali fra un terreno e l'altro
    Ora il terreno può essere creato all'infinito

---
+++ Assets/Terrain/ChunkManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour {
+	
+	public GameObject chunk;
+	
+	public float viewDist;
+	public Vector3 viewPos;
+	
+	public int quadSize;
+	
+	private List<GameObject> spawned;
+	
+	// Use this for initialization
+	void Start () {
+		spawned = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int y = (int)-viewDist; y <= viewDist; y += quadSize){
+			for (int x = (int)-viewDist; x <= viewDist; x += quadSize)
+			{
+				float roundedX = viewPos.x - viewPos.x % quadSize;
+				float roundedZ = viewPos.z - viewPos.z % quadSize;
+				Vector3 roundedPos = new Vector3(roundedX,0,roundedZ);
+				Vector3 currentPos = roundedPos + new Vector3(x - x % quadSize,0,y - y % quadSize);
+				float dist = Vector3.Distance(currentPos, roundedPos);
+				if(dist<viewDist){
+					//check if quad exist
+					GameObject obj = FindObjAtPos(currentPos);
+					if(obj == null){
+						//if it doesn't create it and set offset
+						GameObject chunked = Instantiate(chunk, currentPos, Quaternion.identity);
+						spawned.Add(chunked);
+						chunked.GetComponent<Grid>().offset = new Vector2(currentPos.x, currentPos.z);
+					}
+					else{
+						//if it does, activate it
+						obj.SetActive(true);
+					}
+				}
+			}
+		}
+	}
+	
+	GameObject FindObjAtPos(Vector3 pos){
+		foreach (GameObject obj in spawned)
+		{
+			if(obj.transform.position == pos){
+				return obj;
+			}
+		}
+		return null;
+	}
+	
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(viewPos, viewDist);
+	}
+	
+}
--- Assets/Terrain/Grid.cs
+++ Assets/Terrain/Grid.cs
@@ -16,6 +16,9 @@ public class Grid : MonoBehaviour {
 	private Mesh mesh;
 	private List<Vector3> verts;
 	private List<int> tri;
+	private List<Vector2> uvs;
+	private List<Vector3> norm;
+	private Vector3[] normals;
 	
 	//int mapWidth, int mapHeight, float scale, int seed, int octaves,float persistance, float lacunarity, Vector2 offset, NormalizeMode normalizeMode
 	[Range(0.1f,100f)]
@@ -42,13 +45,27 @@ public class Grid : MonoBehaviour {
 	
 	void OnDrawGizmos(){
 		Setup();
-		float[,] noise = Noise.NoiseMapGenerator(size, size, scale, seed, octaves, persistance, lacunarity, offset, normalize);
-		for(int y = 0; y < size+1; y++){
-			for (int x = 0; x < size+1; x++)
+		float[,] fullNoise = Noise.NoiseMapGenerator(size+2, size+2, scale, seed, octaves, persistance, lacunarity, offset, normalize);
+		
+		float[,] noise = new float[size+1,size+1];
+		for(int y = 1; y <= size+1; y++){
+			for(int x = 1; x <= size+1; x++){
+				noise[x-1,y-1] = fullNoise[x,y];
+			}
+		}
+		
+		
+		
+		for(int y = 0; y <= size; y++){
+			for (int x = 0; x <= size; x++)
 			{
 				float currentHeight = noise[x,y];
+				
+				//verts
 				Vector3 pos = new Vector3(x,currentHeight,y);
 				verts.Add(pos);
+				
+				//tris
 				if(x < size && y < size){
 					int t1 = x + y * size + y;
 					int t2 = t1 + 1;
@@ -61,15 +78,157 @@ public class Grid : MonoBehaviour {
 					tri.Add(t4);
 					tri.Add(t2);
 				}
+				
+				//uvs
+				Vector2 uv = new Vector2(x / size, y / size);
+				uvs.Add(uv);
 			}
 		}
+		
+		
+		
+		List<Vector3> fullVerts = new List<Vector3>();
+		for(int y = 0; y <= size+2; y++){
+			for (int x = 0; x <= size+2; x++)
+			{
+				float fullHeight = fullNoise[x,y];
+				Vector3 pos = new Vector3(x,fullHeight,y);
+				fullVerts.Add(pos);
+			}
+		}
+		
+		/*
+		3-----4
+		|	  |
+		| 	  |
+		1-----2
+		 */
+		
+		Vector3[,] norms = new Vector3[size+3,size+3];
+		for(int y = 0; y < size+2; y++){
+			for (int x = 0; x < size+2; x++)
+			{
+				
+				Vector3 pos1 = new Vector3(x,   fullNoise[x,  y],   y);
+				Vector3 pos2 = new Vector3(x+1, fullNoise[x+1,y],   y);
+				Vector3 pos3 = new Vector3(x,   fullNoise[x,  y+1], y+1);
+				Vector3 pos4 = new Vector3(x+1, fullNoise[x+1,y+1], y+1);
+				
+				Vector3 norm1 = calculateNormal(pos1, pos3, pos2);
+				Vector3 norm2 = calculateNormal(pos4, pos2, pos3);
+				norms[x,   y   ] += norm1;
+				norms[x+1, y   ] += norm1;
+				norms[x,   y+1 ] += norm1;
+				norms[x+1, y   ] += norm2;
+				norms[x,   y+1 ] += norm2;
+				norms[x+1, y+1 ] += norm2;
+			}
+		}
+		
+		
+		int j = 0;
+		for(int y = 1; y <= size+1; y++){
+			for(int x = 1; x <= size+1; x++){
+				normals[j] = norms[x,y];
+				j++;
+			}
+		}
+		
+		for(int i = 0; i < normals.Length; i++){
+			normals[i] = normals[i].normalized;
+		}
+		
 		Create();
+		
+		/*
+		for(int i = 0; i < fullNoise.Length-1; i++){
+			int vertextIndexA = tri[i];
+			int vertextIndexB = tri[i+1];
+			int vertextIndexC = tri[i+2];
+			Vector3 norm = calculateNormal(verts.ToArray(), vertextIndexA, vertextIndexB, vertextIndexC);
+			normals[vertextIndexA] += norm;
+			normals[vertextIndexB] += norm;
+			normals[vertextIndexC] += norm;
+		}
+		 */
+		
+		// print("verts: " + verts.Count);
+		// print("tri:" + tri.Count);
+		// print("norm: " + normals.Length);
+		
+		
+		// for(int i = 0; i < mesh.normals.Length; i++)
+		// {
+		// 	Gizmos.DrawRay(mesh.vertices[i] + transform.position, mesh.normals[i]);
+		// }
+	}
+	
+	/*
+	//calculate normals for right area
+		for(int i = 0; i < tri.Count; i+=3){
+			int vertextIndexA = tri[i];
+			int vertextIndexB = tri[i+1];
+			int vertextIndexC = tri[i+2];
+			Vector3 norm = calculateNormal(verts.ToArray(), vertextIndexA, vertextIndexB, vertextIndexC);
+			normals[vertextIndexA] += norm;
+			normals[vertextIndexB] += norm;
+			normals[vertextIndexC] += norm;
+		}
+		
+		//setup to calculate border normals
+		List<Vector3> fullVerts = new List<Vector3>();
+		for(int y = 0; y <= size+1; y++){
+			for (int x = 0; x < size+1; x++)
+			{
+				float fullHeight = fullNoise[x,y];
+				Vector3 pos = new Vector3(x,fullHeight,y);
+				fullVerts.Add(pos);
+			}
+		}
+		
+		//for every vertex of the border calculate normals
+		for(int y = 0; y <= size+1; y++){
+			for(int x = 0; x <= size+1; x++){
+				int indA = x + y*(size+2);
+				int indB = indA + 1;
+				int indC = indA + y*(size+2);
+				int indD = indC + 1;
+				
+				Vector3 norm1 = calculateNormal(fullVerts.ToArray(), indA, indB, indC);
+				Vector3 norm2 = calculateNormal(fullVerts.ToArray(), indD, indB, indC);
+			}
+		}
+		
+		for(int i = 0; i < normals.Length; i++){
+			normals[i] = normals[i].normalized;
+		}
+	 */
+	 
+	Vector3 calculateNormal(Vector3 a, Vector3 b, Vector3 c){
+		
+		Vector3 sideAB = b - a;
+		Vector3 sideAC = c - a;
+		return Vector3.Cross(sideAB,sideAC).normalized;
+	} 
+	
+	
+	Vector3 calculateNormal(Vector3[] list, int a, int b, int c){
+		Vector3 pointA = list[a];
+		Vector3 pointB = list[b];
+		Vector3 pointC = list[c];
+		
+		Vector3 sideAB = pointB - pointA;
+		Vector3 sideAC = pointC - pointA;
+		return Vector3.Cross(sideAB,sideAC).normalized;
 	}
 	
 	void Create(){
 		mesh.vertices = verts.ToArray();
 		mesh.triangles = tri.ToArray();
-		mesh.RecalculateNormals();
+		mesh.uv = uvs.ToArray();
+		mesh.normals = normals;
+		mesh.RecalculateTangents();
+		mesh.RecalculateBounds();
 		GetComponent<MeshFilter>().mesh = mesh;
 	}
 	
@@ -78,7 +237,11 @@ public class Grid : MonoBehaviour {
 		
 		verts = new List<Vector3>();
 		tri = new List<int>();
+		uvs = new List<Vector2>();
+		normals = new Vector3[(size+1)*(size+1)];
+		norm = new List<Vector3>();
 		
 		mesh = new Mesh();
+		mesh.name = "Chunk";
 	}
 }
